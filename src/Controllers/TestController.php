67eec1e4b9a34edf6974bb8ee6fe4066b260ec1b
Guilherme Abel <guilhermeabel8@gmail.com>
2024-05-03 23:37:47 -0300

    remove: test data

--- src/Controllers/TestController.php
+++ src/Controllers/TestController.php
@@ -2,37 +2,11 @@
 
 namespace App\Controllers;
 
-use mysqli;
 
 class TestController extends BaseController {
 
 	public function index() {
-		try {
-			$ola = "Ola Mundo!";
 
-
-			for ($i=1 ; $i<11 ; $i++){
-
-						  echo $ola;
-
-						  if($i%2 == 0){
-
-										 echo "<br>";
-
-						   }
-
-						   else{
-
-										  echo " - ";
-
-						   }
-
-			  }
-
-
-		} catch (\Throwable $e) {
-			echo 'catched exception: ' . $e->getMessage() . '<br>';
-		}
 	}
 	
 }
