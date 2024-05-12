bf4dc66203e2ca00040d60edf070a9e270696408
Guilherme Abel <guilhermeabel8@gmail.com>
2024-05-12 00:35:03 -0300

    feat: using slim for routing

--- public/index.php
+++ public/index.php
@@ -4,16 +4,40 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../src/config.php';
 
-use App\Controllers\TestController;
-use App\Controllers\UserController;
-use App\Web\Router;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-$userController = new UserController();
-$testController = new TestController();
+require __DIR__ . '/../vendor/autoload.php';
 
-$router = new Router();
-$router->get('/signup', static fn () => $userController->create());
-$router->post('/signup', static fn () => $userController->store());
-$router->get('/test', static fn () => $testController->index());
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+$app->addBodyParsingMiddleware();
+$app->addErrorMiddleware(true, true, true);
 
-$router->handleRequest();
+$logMiddleware = static function (Request $request, $handler) {
+    $response = $handler->handle($request);
+    $requestBody = $request->getParsedBody();
+    $responseBody = (string) $response->getBody();
+
+    file_put_contents(__DIR__ . '/../logs/app.requests.log', sprintf(
+        "[%s] %s %s %s %s\n",
+        date('Y-m-d H:i:s'),
+        $request->getMethod(),
+        $request->getUri()->getPath(),
+        json_encode($requestBody),
+        $responseBody
+    ), FILE_APPEND);
+
+    return $response;
+};
+
+// $app->add($logMiddleware);
+
+$app->get('/', static function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Hello world!');
+
+    return $response;
+});
+
+$app->run();
