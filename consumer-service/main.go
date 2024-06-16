b80a1a4424caa995fdfc9a9e681c0da58d430bc3
Guilherme Abel <guilhermeabel8@gmail.com>
2024-06-16 18:44:37 -0300

    feat: mongodb connection configuration in go service

--- consumer-service/main.go
+++ consumer-service/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const version = "1.0.0"
 
 type config struct {
-	port int
-	env  string
+	port  int
+	env   string
+	dbURI string
 }
 
 type App struct {
@@ -24,6 +32,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.dbURI, "db-uri", os.Getenv("MONGO_CONNECTION_URI"), "Database URI")
 	flag.Parse()
 
 	logger := log.New(log.Writer(), "CONSUMER-SERVICE: ", log.LstdFlags)
@@ -33,11 +42,36 @@ func main() {
 		logger: logger,
 	}
 
-	err := app.NewServer().ListenAndServe()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+
+	mongoClient, err := mongo.Connect(
+		ctx,
+		options.Client().ApplyURI(app.config.dbURI),
+	)
+
+	if err != nil {
+		app.logger.Fatalf("mongodb connection error : %v", err)
+	}
+
+	defer func() {
+		cancel()
+		if err := mongoClient.Disconnect(ctx); err != nil {
+			log.Fatalf("mongodb disconnect error : %v", err)
+		}
+	}()
+
+	err = mongoClient.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	err = app.NewServer().ListenAndServe()
 
 	if err != nil {
 		println("Error during consumer service startup")
-		log.Fatal(err)
+		app.logger.Fatal(err)
 	}
 
 	println("Consumer service started")
